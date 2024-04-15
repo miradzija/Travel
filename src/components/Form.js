c02545b2295d22f110f13e4d705c5354c6c6530a
Milos Zivanovic <zivanovicmilos86@gmail.com>
2024-04-15 23:29:33 +0200

    finish

---
+++ src/components/Form.js
@@ -0,0 +1,46 @@
+import { useState } from "react";
+
+export default function Form({ onAddItems, setItems }) {
+  const [description, setDescription] = useState("");
+  const [quantity, setQuantity] = useState(1);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    if (!description) return;
+
+    const newItem = { description, quantity, packed: false, id: Date.now() };
+    console.log(newItem);
+
+    onAddItems(newItem);
+
+    setDescription("");
+    setQuantity(1);
+  }
+
+  return (
+    <form className="add-form" onSubmit={handleSubmit}>
+      <h3>Sta mi treba za put?</h3>
+      <select
+        value={quantity}
+        onChange={(e) => setQuantity(Number(e.target.value))}
+      >
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+          <option value={num} key={num}>
+            {num}
+          </option>
+        ))}
+      </select>
+      <input
+        type="text"
+        placeholder="Item..."
+        value={description}
+        onChange={(e) => {
+          // console.log(e.target.value);
+          setDescription(e.target.value);
+        }}
+      ></input>
+      <button>Add</button>
+    </form>
+  );
+}
